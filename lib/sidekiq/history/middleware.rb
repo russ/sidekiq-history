8e934d9cc7fd5eeb8fccede58bda144913fe64b5
Robert Coleman <github@robert.net.nz>
2015-04-10 22:56:30 +1200

    migrate redis list to set

--- lib/sidekiq/history/middleware.rb
+++ lib/sidekiq/history/middleware.rb
@@ -19,6 +19,18 @@ module Sidekiq
         }
 
         Sidekiq.redis do |conn|
+          # migration of list to set for backwards compatibility after v0.0.4
+          if conn.type(LIST_KEY) == 'list'
+            length = conn.llen(LIST_KEY)
+            list = conn.lrange(LIST_KEY, 0, length)
+            conn.del(LIST_KEY)
+            list.each do |entry|
+              migrated_data = JSON.parse(entry)
+              conn.zadd(LIST_KEY, data[:started_at].to_f, Sidekiq.dump_json(migrated_data))
+            end
+          end
+
+          # regular storage of history
           conn.zadd(LIST_KEY, data[:started_at].to_f, Sidekiq.dump_json(data))
           unless Sidekiq.history_max_count == false
             conn.zremrangebyrank(LIST_KEY, 0, -(Sidekiq.history_max_count + 1))
